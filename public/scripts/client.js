26d1c98739699b56a631de63b4dd713e35664829
Nancy Boktor <nancyboktor1@gmail.com>
2021-10-06 22:48:55 -0400

    Geting all tweets from data base & Stop refreshing the page while making post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,39 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(() => {
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    createdAt: 1461116232227,
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function (tweets) {
+        console.log("data:", tweets);
+        renderTweets(tweets);
+      },
+    });
   };
-
-  const createTweetElement = function (tweetData) {
-    let $tweet = $(`<article class="tweets_container">
+  loadTweets();
+  const createTweetElement = function (tweet) {
+    return `<article class="tweets_container">
           <header class="tweetbox_header">
              <div class="user"> 
-                <h5 id="name">${tweetData.user.name}</h5>
-                <img class="userImage" src=${tweetData.user.avatars} alt="">
+                <h5 id="name">${tweet.user.name}</h5>
+                <img class="userImage" src=${tweet.user.avatars} alt="">
             </div>
-            <div class="atUserName"><h5>${tweetData.user.handle}</h5></div>
+            <div class="atUserName"><h5>${tweet.user.handle}</h5></div>
           </header>
-          <div class="myPost"><h5>${tweetData.content.text}</h5></div> 
+          <div class="myPost"><h5>${tweet.content.text}</h5></div> 
           <footer class="tweetbox_footer">
-             <div class="forDays">${timeago.format(tweetData.createdAt)} </div>
+             <div class="forDays">${timeago.format(tweet.createdAt)} </div>
              <div class="hover_state">
                <i class="fas fa-flag"></i>
                <i class="fas fa-retweet"></i>
                <i class="fas fa-heart"></i>
              </div>
           </footer>
-        </article>`);
-    return $tweet;
+        </article>`;
   };
-  const $tweet = createTweetElement(tweetData);
-  $(".tweet_box").append($tweet);
+
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      const tweetDiv = createTweetElement(tweet);
+      $(".tweet_box").append(tweetDiv);
+    }
+  };
+
+  $(".main-form").submit(function (event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+
+    $.ajax({ url: "/tweets", method: "POST", data: $(this).serialize() }).then(
+      function (res) {
+        console.log("Success: ", res);
+        const tweets = loadTweets();
+        console.log(tweets);
+        renderTweets(tweets);
+      }
+    );
+  });
 });
--- public/scripts/test.js
+++
@@ -1,9 +0,0 @@
-console.log("Good");
-// const $name = $("<h5>").text(tweetData["user"].name);
-  // const $userImage = $("<img>").image(tweetData["user"].avatars);
-  // const $atUserName = $("<div>", "<h5>").image(tweetData["user"].handel);
-  // const $myPost = $("<div>", "<h5>").text(tweetData["content"].text);
-  // const $forDays = $("<div>").text(tweetData.created_at);
-  // const $post = $("<artical>").addClass("tweets_container");
-  // $post.append($name, $userImage, $atUserName, $myPost, $forDays);
-  // return $post;
